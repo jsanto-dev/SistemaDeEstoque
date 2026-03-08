ba4f6971654e9b9c3ef7cad756960abb365d3dda
jsanto-dev <jsanto.dev@gmail.com>
2026-03-08 13:13:23 -0300

    Configuração estrutural da base do projeto e arquivos de stilos

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -15,8 +15,8 @@ public class ConnectionFactory {
         private static final String PASSWORD = "cimatec";
     //Métodos
         
-        public static Connection getConnection(){
-            Connection con = null;
+        public static connection getConnection(){
+            connection con = null;
             
             try {
                 class.forName(DRIVER);
